160829aa9fd021a0c317bff810c1873b4a86eac2
ram <qwerty@gmail.com>
2022-02-20 10:54:27 +0530

    mycommit

---
+++ Evenoddnumber/src/mypackage/EvenOdd.java
@@ -0,0 +1,23 @@
+package mypackage;
+import java.util.Scanner;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		  Scanner sc  =  new Scanner(System.in);
+		  
+		  
+		   int n  =  sc.nextInt();
+		   System.out.println("Enter the Number"); 
+		   if(n%2==0)
+		   
+			  System.out.println("Number is Even");
+		   
+		   else
+			   System.out.println("Number is Odd");
+		
+	}
+
+}
---
+++ Program1Hello5times/src/mypackage/Hello5Times.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+public class Hello5Times {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		for (int i=1;i<=5;i++)
+		{
+			System.out.println("Hello");
+		}
+		
+
+	}
+
+}
